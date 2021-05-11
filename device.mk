33c68bd7888efe39f2434f72ab11c4076adf5bc4
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-11 18:39:47 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -404,8 +404,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.impl.so:64 \
+    vendor.qti.hardware.vibrator.service
 
 # VNDK
 PRODUCT_PACKAGES += \
